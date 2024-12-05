aaedc5ac1e0c52edfb39d8c1700da08cca9aa152
rodr0245 <rodr0245@algonquinlive.com>
2024-12-04 22:54:29 -0500

    deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static', 
-  site: 'https://rodr0245.github.io/mtm6407-static-site-rodr0245',
+  site: 'https://rodr0245.github.io/mtm6407-static-site-rodr0245', // Correct URL for GitHub Pages
   build: {
-    assets: 'dist/',
     emptyOutputDir: true,
   },
 });
